c3f1e6c44915c27d0acdf1863cebc0492f29d788
Sean Klein <sean@oxidecomputer.com>
2026-04-15 17:07:43 -0700

    Fix runtime linking: set RPATH via .cargo/config.toml
    cargo:rustc-link-arg in a library crate's build.rs does not propagate
    to downstream binaries. Move the RPATH for /usr/lib/fm/amd64 to
    .cargo/config.toml so test and binary targets in the workspace can
    find libfmd_adm.so.1 at runtime.

--- fmd-adm-sys/build.rs
+++ fmd-adm-sys/build.rs
@@ -1,5 +1,4 @@
 fn main() {
     println!("cargo:rustc-link-lib=fmd_adm");
     println!("cargo:rustc-link-search=native=/usr/lib/fm/amd64");
-    println!("cargo:rustc-link-arg=-R/usr/lib/fm/amd64");
 }
